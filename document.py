e78123506ab46c938e0cf4f2547c45d9490ebeeb
ribsthakkar <rishabh.thakkar@gmail.com>
2019-02-02 20:04:13 -0600

    Added pre-computed models and created Document and Classifier objects

---
+++ document.py
@@ -0,0 +1,27 @@
+import hashlib
+
+class Document:
+    def __init__(self, title=None, text=None):
+        if title == None or text == None:
+            self.__title = None
+            self.__body = None
+            self.__value = None
+            self.__hash = None
+        else:
+            self.__title = title
+            self.__text = text
+    def __hash__(self):
+        if self.__hash:
+            return self.__hash
+        else:
+            h = hashlib.md5()
+            h.update(bytes(self.__title))
+            h.update(bytes(self.__text))
+            self.__hash = h.hexdigest()
+            return self.__hash
+    def __compute_val(self, classifier):
+        if self.__value:
+            return self.__value
+        else:
+            self.__value = classifier.classify(self.__title, self.__text)
+            return self.__value
