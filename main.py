c044fefcf0645e7619c7e8922f17fd4447264a17
ribsthakkar <rishabh.thakkar@gmail.com>
2019-02-11 00:15:13 -0600

    Try revert to old classification impl

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def check():
         else:
             return render_template('failure.html')
     except Exception as e:
-        # raise e
+        print(e)
         return render_template('error.html')
     # hashed_val = hash(d)
     # if hashed_val in database:
